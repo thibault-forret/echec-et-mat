832da509d2b4076bd62e0cdc77194061ea2e093e
quentin-massoulle <quentin.massoulledu80@gmail.com>
2024-10-02 18:26:16 +0200

    creation class piece

---
+++ js/class/piece.js
@@ -0,0 +1,7 @@
+class pieces{
+    
+    constructor(){
+        emplacements={};//emplacements des pièces
+        deplacements={};//deplacements des pièces en fonction de leur emplacement 
+    }
+}
