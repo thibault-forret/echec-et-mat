39cb2fcf0037cb1fe53171dd40a663bf04d71ae0
thibault_f <78164277+thibault-forret@users.noreply.github.com>
2024-10-02 15:52:44 +0200

    add js folder

---
+++ js/generate_chessboard.js
@@ -0,0 +1,10 @@
+const chessboard = document.querySelector('.chessboard');
+
+for (let row = 0; row < 8; row++) {
+    for (let col = 0; col < 8; col++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.classList.add((row + col) % 2 === 0 ? 'white' : 'black');
+        chessboard.appendChild(square);
+    }
+}
