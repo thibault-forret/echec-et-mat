c1abc60317701788ff9f0e65fe1c392a86f84d71
quentin-massoulle <quentin.massoulledu80@gmail.com>
2024-10-02 19:01:42 +0200

    ajoue  img pour le constructeur

--- js/class/roi.js
+++ js/class/roi.js
@@ -1,5 +1,5 @@
 class King extends Pieces {     
-    constructor(emplacement, color) {
+    constructor(imgKing ,emplacement, color) {
         super(imgKing, color, "King");
         this.emplacement = emplacement;
     }
