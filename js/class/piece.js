848d4aa613772636c9a45e453bc9d608c43af2c4
quentin-massoulle <quentin.massoulledu80@gmail.com>
2024-10-02 18:31:20 +0200

    creation crostructor

--- js/class/piece.js
+++ js/class/piece.js
@@ -1,7 +1,10 @@
 class pieces{
     
-    constructor(){
-        emplacements={};//emplacements des pièces
-        deplacements={};//deplacements des pièces en fonction de leur emplacement 
+    constructor(image,color,type){
+        this.emplacement = [];//emplacement de la  pièces
+        this.deplacements=[];//deplacements des pièces en fonction de leur emplacement
+        this.image = image;//image de la pièce
+        this.color = color;//couleur de la pièce
+        this.type = type;//type de la pièce(rois, reine, ect...)
     }
 }
