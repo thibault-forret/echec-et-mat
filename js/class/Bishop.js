836c49bac0014321629501b6404bdd54097e0eab
DuboissetHugo <duboissethugo@gmail.com>
2024-10-15 18:15:37 +0200

    Création de l'ensemble des classes

---
+++ js/class/Bishop.js
@@ -0,0 +1,20 @@
+class Bishop extends Pieces {     
+    constructor(imgBishop ,emplacement, color) {
+        super(imgBishop, color, "Bishop");
+        this.emplacement = emplacement;
+    }
+
+    // Déplacements du fou : uniquement en diagonale
+    checkMove(){
+        const [row, col] = this.emplacement;
+        const moves = [];
+
+        // Diagonales
+        for (let i = 1; i < 8; i++) {
+            moves.push([row + i, col + i], [row - i, col - i], [row + i, col - i], [row - i, col + i]);
+        }
+
+        return this.filterValidMoves(moves, board);
+    }
+
+}
---
+++ js/class/Knight.js
@@ -0,0 +1,18 @@
+class Knight extends Pieces {     
+    constructor(imgKnight ,emplacement, color) {
+        super(imgKnight, color, "Knight");
+        this.emplacement = emplacement;
+    }
+
+    // Déplacement en L
+    checkMove(){
+        const [row, col] = this.emplacement;
+        const moves = [
+            [row + 2, col + 1], [row + 2, col - 1], [row - 2, col + 1], [row - 2, col - 1],
+            [row + 1, col + 2], [row + 1, col - 2], [row - 1, col + 2], [row - 1, col - 2]
+        ];
+
+        return this.filterValidMoves(moves, board);
+    }
+
+}
