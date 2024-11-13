ceddb4e13f7f49d5026ea06a38dd4b5323c51715
thibault-forret <forret.thibault@gmail.com>
2024-11-13 19:32:05 +0100

    update rook move

--- js/class/Rook.js
+++ js/class/Rook.js
@@ -6,21 +6,10 @@ export default  class Rook extends Pieces
     {
         super(imgRook, color, "Rook");
         this.emplacement = emplacement;
-    }
-
-    // Déplacements de la tour : uniquement sur les lignes et colonnes
-    checkMove(board)
-    {
-        const [row, col] = this.emplacement;
-        const moves = [];
-
-        // lignes verticales et horizontales
-        for (let i = 1; i < 8; i++) {
-            moves.push([row + i, col], [row - i, col], [row, col + i], [row, col - i]);
-        }
-    
-
-        return this.filterValidMoves(moves, board);
+        this.directions = [
+            [1, 0], [-1, 0], [0, 1], [0, -1]
+        ];
+        this.maxSteps = 7; 
     }
 
 }
